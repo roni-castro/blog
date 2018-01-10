3d8ab399427af55763ec0072ca730cbec0cf789e
Roni Cesar de Castro <ronicesardecastro@gmail.com>
2018-01-10 04:06:47 +0000

    Add update and delete post option

--- app.js
+++ app.js
@@ -1,64 +1,115 @@
-var express = require("express"),
-    bodyParser = require("body-parser"),
-    mongoose = require("mongoose"),
-    app = express();
+var express             = require("express"),
+    expressSanitizer    = require("express-sanitizer"),
+    bodyParser          = require("body-parser"),
+    mongoose            = require("mongoose"),
+    methodOverride      = require("method-override"),
+    app                 = express();
     
 mongoose.connect("mongodb://localhost/blog_db", {useMongoClient: true});
 var db = mongoose.connection;
-
 // When successfully connected
 db.on('connected', function() {
     console.log('Mongo DB connection open for DB');
 });
+
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
 
-var blogSchema = new mongoose.Schema({
+var postSchema = new mongoose.Schema({
     title: String,
     body: String,
     photo: String,
     createAt: {type:Date, default: Date.now}
 });
-var Blog = mongoose.model("Blog", blogSchema);
+var Blog = mongoose.model("Blog", postSchema);
     
 app.get("/", function(req, res){
-    res.redirect("/blog");
+    res.redirect("/posts");
 })
 
-app.get("/blog", function(req, res){
+app.get("/posts", function(req, res){
    Blog.find({}, function(err, posts){
        if(err){
            console.log(err);
        } else{
            console.log(posts);
-           res.render("blog", {posts: posts});
+           res.render("posts", {posts: posts});
        }
     });
 });
 
-app.get("/blog/new", function(req, res){
+app.get("/posts/new", function(req, res){
     res.render("new");
 });
 
-app.get("/blog/edit", function(req, res){
-    res.render("edit");
+app.get("/posts/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, post){
+       if(err){
+           console.log(err);
+       } else{
+           console.log(post);
+           res.render("edit", {post: post});
+       }
+    });
 });
 
-app.get("/blog/:id", function(req, res){
-    res.render("blogDetail");
+app.get("/posts/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, post){
+       if(err){
+           console.log(err);
+       } else{
+           console.log(post);
+           res.render("postDetail", {post: post});
+       }
+    });
 });
 
-app.post("/blog", function(req, res){
-    var blog = req.body.post;
-    console.log(blog);
-    saveNewPostToDB(blog);
-     res.redirect("/blog");
+app.post("/posts", function(req, res){
+     req.body.post.body = req.sanitize(req.body.post.body);
+    var post = req.body.post;
+    saveNewPostToDB(post);
+    res.redirect("/posts");
 });
+
+app.put("/posts/:id", function(req, res){
+    req.body.post.body = req.sanitize(req.body.post.body);
+    var post = req.body.post;
+    console.log("PUT");
+    console.log(post);
+     Blog.findByIdAndUpdate(req.params.id, post, function(err, updatedPost){
+         if(err){
+            console.log(err);
+        } else {
+            console.log("Post updated");
+            console.log(updatedPost);
+            res.redirect("/posts");
+        }
+    });
+    
+});
+
+app.delete("/posts/:id", function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+         if(err){
+            console.log(err);
+        } else {
+            console.log("Post updated");
+            res.redirect("/posts");
+        }
+    });
+});
+
+app.get("*", function(req, res) {
+    res.send("Route not found");
+})
     
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is listening");
-})
+});
 
 function saveNewPostToDB(jsonValue){
     Blog.create(jsonValue, 
